76bbcd342a6498c8f54a77b41a364d953ef6c0da
FriendlyJesse <jessexinyu@foxmail.com>
2017-11-28 23:07:25 +0800

    增加 Fastclick 消除移动端 300ms 延迟

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import FastClick from 'fastclick'
 
 import './common/scss/app.scss'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+FastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
